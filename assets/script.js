26c0e91c0944be0ee28c77e6ef8099fb836d9900
Marshall <marshall.peters01@gmail.com>
2023-03-20 14:29:18 -0400

    added hide/show events, timers, quiz begin

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,35 @@
-var startButton = $('#start');
+var startButton = document.getElementById('start');
+var timerEl = document.getElementById('timer');
+var questionList = [
+  {
+  Question:"What is your name?", 
+  Answers: ["A. Steve", "B. Joe", "C. Manny", "D. John"]
+  }
+]
 
-function quizBegin() {
-    
+function countdown() {
+    var timeLeft = 120;
+    timerEl.textContent = "Time Remaining: " + 120;
+    var timeInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = "Time Remaining: " + timeLeft;
+        if(timeLeft == -1){
+          clearInterval(timeInterval);
+          timerEl.textContent = " ";
+          displayMessage();
+        }
+    }, 1000);
+  }
+
+function showQuestion() {
+  var questionContent = document.querySelector('#question-content');
+  questionContent.classList.remove('hidden');
 }
 
-startButton.addEventListener("click", quizBegin)
+function quizBegin() {
+    startButton.classList.add('hidden');
+    showQuestion();
+    countdown();
+}
+
+startButton.addEventListener("click", quizBegin);
